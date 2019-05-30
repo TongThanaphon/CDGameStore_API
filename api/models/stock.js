202b21ec60f7faa9599b812e121e0898b9c15f92
ThanaphonKeawjam <tongasura001@gmail.com>
2019-05-30 21:51:18 +0700

    update stocks

--- api/models/stock.js
+++ api/models/stock.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const stockSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-   itemId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product DLC', required: true },
+    itemId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product DLC', required: true },
     quantity: { type: Number, default: 1 },
     type: { type: String, required: true }
 });
