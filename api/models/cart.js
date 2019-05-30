09f3b812cb239a879e56faca8ef42d9f7ae0b654
ThanaphonKeawjam <tongasura001@gmail.com>
2019-05-30 15:07:33 +0700

    fix cart routes

--- api/models/cart.js
+++ api/models/cart.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cartSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     stock: { type: mongoose.Schema.Types.ObjectId, ref: 'Stock', required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: 'Guest' },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     quantity: { type: Number, required: true }
 });
 
