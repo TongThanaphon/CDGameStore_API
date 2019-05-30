a832e7b7c0cacce67733fd98f9a586fdf0b77d40
ThanaphonKeawjam <tongasura001@gmail.com>
2019-05-30 14:06:08 +0700

    update history route

--- api/models/history.js
+++ api/models/history.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const historySchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    product: { type: Map, of: String},
-    dlc: { type: Map, of: String },
-    date: { type: Map, of: String }
+    item: { type: mongoose.Schema.Types.ObjectId, ref: 'Product DLC' },
+    quantity: { type: Number },
+    date: { type: Map, of: Number }
 });
 
 module.exports = mongoose.model('History', historySchema);
--- api/routes/historys.js
+++ api/routes/historys.js
@@ -9,14 +9,8 @@ router.post('/create', (req, res, next) => {
     const history = new History({
         _id: new mongoose.Types.ObjectId(),
         userId: req.body.userId,
-        product: {
-            productId: req.body.product.productId,
-            quantity: req.body.product.quantity
-        },
-        dlc: {
-            dlcId: req.body.dlc,
-            quantity: req.body.dlc.quantity
-        },
+        item: req.body.item,
+        quantity: req.body.quantity,
         date: {
             year: req.body.date.year,
             month: req.body.date.month,
@@ -32,8 +26,8 @@ router.post('/create', (req, res, next) => {
             history: {
                 _id: result._id,
                 userId: result.userId,
-                product: result.product,
-                dlc: result.dlc,
+                item: req.body.item,
+                quantity: req.body.quantity,
                 date: result.date
             }
         });
@@ -56,8 +50,8 @@ router.get('/', (req, res, next) => {
                 return {
                     _id: doc._id,
                     userId: doc.userId,
-                    product: doc.product,
-                    dlc: doc.dlc,
+                    item: doc.item,
+                    quantity: doc.quantity,
                     date: doc.date
                 };
 
@@ -74,17 +68,19 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.get('/findByProductId/:productId', (req, res, next) => {
-    const id = req.params.productId;
+router.get('/findByItemId/:itemId', (req, res, next) => {
+    const id = req.params.itemId;
     
-    History.find({ product: { productId: id } })
+    History.find({ item: id })
     .exec()
     .then(docs => {
         const response = {
             count: docs.length,
             product: docs.map(doc => {
                 return {
-                    product: doc.product,
+                    userId: doc.userId,
+                    item: doc.product,
+                    quantity: doc.quantity,
                     date: doc.date
                 };
             })
@@ -99,32 +95,7 @@ router.get('/findByProductId/:productId', (req, res, next) => {
     });
 });
 
-router.get('/findByDLCId/:dlcId', (req, res, next) => {
-    const id = req.params.dlcId;
-    
-    History.find({ dlc: { dlcId: id } })
-    .exec()
-    .then(docs => {
-        const response = {
-            count: docs.length,
-            product: docs.map(doc => {
-                return {
-                    dlc: doc.dlc,
-                    date: doc.date
-                };
-            })
-        };
-        res.status(200).json(response);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
-
-router.get('findByUserId/:userId', checkAuth, (req, res, next) => {
+router.get('/findByUserId/:userId', checkAuth, (req, res, next) => {
     const id = req.params.userId;
 
     History.find({ userId: id })
